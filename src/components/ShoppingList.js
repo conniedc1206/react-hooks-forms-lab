32e89f654ee33652b9b6c97da81c5c9262bc3d50
Connie Park <conniedc1206@gmail.com>
2022-06-13 08:56:37 -0700

    commit tree 59ea4440724b2f03f693f375337a875141d988f0

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,29 +2,57 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
+  //console.log(items);
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+  const [itemArray, setItemArray] = useState(items);
 
+  const itemsToDisplay = itemArray
+    .filter((item) => {
+      if (selectedCategory === "All") {
+        return true;
+      } else {
+        return item.category === selectedCategory;
+      }
+    })
+    .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+
+  console.log(itemsToDisplay);
+
+  
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+    setItemArray((itemArray) => (itemArray = itemsToDisplay));
   }
-
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
-
+  console.log(itemArray);
+  
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    setItemArray((itemArray) => (itemArray = itemsToDisplay));
+  };
+  //console.log(itemArray);
+  
+  const onItemFormSubmit = (itemObj) => {
+    setItemArray([...itemArray, itemObj]);
+  };
+  //console.log(itemArray);
+  
+  const itemCard = itemArray.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ));
+  console.log(itemCard)
+  
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+      />
+      <ul className="Items">{itemCard}</ul>
     </div>
   );
 }
